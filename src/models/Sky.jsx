a23c909b3602725c62828a6ba13efd090a164af7
Kristers Davis Gruzins <gruz0001@hz.nl>
2025-10-08 14:10:00 +0200

    Added Island and Sky

---
+++ src/models/Sky.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import skyScene from '../assets/3d/sky.glb'
+
+const Sky = () => {
+  const sky = useGLTF(skyScene);
+  return (
+    <mesh>
+        <primitive object={sky.scene} scale={0.1} position={[0, -1, 0]} />  
+    </mesh>
+  )
+}
+
+export default Sky
