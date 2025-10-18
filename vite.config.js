911b20ee08a0ffe31560f29e35fe5fb58e1130f1
Kristers Davis Gruzins <gruz0001@hz.nl>
2025-10-18 19:00:38 +0200

    updated github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Reactand3js/', 
   assetsInclude: ['**/*.glb'],
 })
